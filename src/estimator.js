0ee1f831b44d8f210cdfc3fc492b149387745525
CrazzySon <0715961g@students.wits.ac.za>
2020-04-13 04:08:17 -0700

    challenge 1-3

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,10 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    const estimator = data;
+    return stimator({
+        data,
+        impart: {},
+        serveImpart: {}
+    })
+};
 
 export default covid19ImpactEstimator;
